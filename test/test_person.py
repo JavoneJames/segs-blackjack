9f83cc49e43a3ee75627d18f2c688e1cd1ad0d3d
Javone <javonejames@gmail.com>
2025-01-16 20:52:01 +0000

    test - schema for PersonTestCase

---
+++ test/test_person.py
@@ -0,0 +1,22 @@
+import unittest
+from src.person import Person
+
+class PersonTestCase(unittest.TestCase):
+
+    def setUp(self):  # this method will be run before each test
+        self.person = Person()
+
+    def tearDown(self):  # this method will be run after each test
+        pass
+    
+    # test that a person with no cards has a score of 0
+    def test_empty_hand(self):
+        pass
+
+    # test that a person with 2 cards has the correct score
+    def test_two_cards(self):
+        pass
+    
+    # test that the score is calculated for multiple cards
+    def test_multiple_cards(self):
+        pass
