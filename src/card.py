8b24e677d52b3665d88676af0b83b889ebc4970e
Javone <javonejames@gmail.com>
2025-01-16 19:45:35 +0000

    implemented - get_value method that returns the face value/worth of a card

--- src/card.py
+++ src/card.py
@@ -3,4 +3,12 @@ class Card:
     def __init__(self, rank):
         self.rank = rank
 
-    
+    # return the value of a card
+    def get_value(self):
+        face_cards = ('J', 'Q', 'K')
+        if self.rank in face_cards:
+            return 10
+        elif self.rank  == 'A':
+            return 11
+        else:
+            return int(self.rank)
