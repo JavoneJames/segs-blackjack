c2a4729d121c6ccd72a9e5e7c5e10c43d6a80e39
Javone <javonejames@gmail.com>
2025-01-16 21:35:14 +0000

    feat - added shuffle_deck method to call shuffle_deck method from deck class - for calrity

--- src/dealer.py
+++ src/dealer.py
@@ -5,3 +5,7 @@ class Dealer(Person):
     def __init__(self, deck):
         super().__init__()
         self.deck = deck
+
+    # calls shuffle_deck method from Deck to shuffle cards
+    def shuffle_deck(self):
+        self.deck.shuffle_deck()
