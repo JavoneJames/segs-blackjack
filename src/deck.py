e3f48e190b62649ec649ff0e44bb98d14f16a345
Javone <javonejames@gmail.com>
2025-01-16 20:08:47 +0000

    feat - added shuffle_deck method that uses the shuffle meothod from the random module to shuffle card objs in random order in-place

--- src/deck.py
+++ src/deck.py
@@ -1,3 +1,15 @@
+from src.card import Card
+from random import shuffle
+
+
 class Deck:
+
     def __init__(self):
-        self.cards = [1] * 52
+        self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        self.cards = [Card(rank) for rank in self.ranks for _ in range(4)]
+
+    # Shuffle deck of cards
+    def shuffle_deck(self):
+        print("Dealer shuffling cards...")
+        shuffle(self.cards)
+
