0d1e9fc818123354233cbfe72b4c11567c72eb0f
Javone <javonejames@gmail.com>
2025-01-16 21:02:58 +0000

    feat - added score method to calc a person's total score based on the card they currently hold when this method is called

--- src/person.py
+++ src/person.py
@@ -6,3 +6,20 @@ class Person:
     # add a card to the person's hand
     def retrieve_card(self, card):
         self.cards.append(card)
+    
+    # calc score for the current hand held by a perosn
+    def score(self):
+        total = 0
+        ace_count = 0
+        # loop through cards held by a person and add face value to total
+        for card in self.cards:
+            card_value = card.get_value()
+            total += card_value
+            if card.rank == 'A': # if card is an ace incerement counter
+                ace_count += 1
+        # loop is valid is total over 21 and ace counter greater than 0
+        while total > 21 and ace_count:
+            total -= 10  # convert Ace from 11 to 1
+            ace_count -= 1
+
+        return total
