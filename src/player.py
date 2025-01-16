72e328fa58a515a9b1c85c3f72d5b8141679cb7a
Javone <javonejames@gmail.com>
2025-01-16 23:10:45 +0000

    feat - added update_balance method to update player balance after win or lost

--- src/player.py
+++ src/player.py
@@ -2,20 +2,27 @@ from src.person import Person
 
 class Player(Person):
 
-    def __init__(self, dealer, avaiable_balance = 1000):
+    def __init__(self, dealer, available_balance = 1000):
         super().__init__()
         self.dealer = dealer
-        self.avaiable_balance = avaiable_balance
+        self.available_balance = available_balance
         self.wager = 0
 
     # return the player's available balance
     def balance(self):
-        return self.avaiable_balance
+        return self.available_balance
     
     # allow the player to set the wager amount
     def set_wager(self, wager_amount):
         self.wager = wager_amount
-    
+
+    # update player balance after winning or lossing a game
+    def update_balance(self, win):
+        if win:
+            self.available_balance += (self.wager * 2)
+        else:
+            self.available_balance -= self.wager
+        self.wager = 0
     # ask the player whether or not they want to hit or stand
     def choose_action(self):
         user_input = input("Would you like to [H]it or [S]tand? ").lower()
