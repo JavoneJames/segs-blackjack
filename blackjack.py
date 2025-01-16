add78a1eda085b98245bce060a6c3b7e04662760
Javone <javonejames@gmail.com>
2025-01-16 22:30:55 +0000

    feat - added set_up method to init game mechanics

--- blackjack.py
+++ blackjack.py
@@ -8,5 +8,19 @@ class BlackJack:
         self.dealer = Dealer(self.deck)
         self.player = Player(self.dealer)
 
+    def set_up_game(self):
+        self.collect_wagers()
+        self.dealer.shuffle_deck()
+        # deal two cards to the player and two cards to the dealer
+        self.player.retrieve_card(self.dealer.deal_card())  # player gets card
+        self.dealer.retrieve_card(self.dealer.deal_card())  # dealer gets card (1st card)
+        self.player.retrieve_card(self.dealer.deal_card())  # player gets card
+        self.dealer.retrieve_card(self.dealer.deal_card())  # dealer gets card (2nd card)
+        # show the initial cards and scores
+        self.show_initial_cards()
+
 if __name__ == '__main__':
-    play()
+    bj = BlackJack()
+    bj.set_up_game()
+    bj.play()
+
